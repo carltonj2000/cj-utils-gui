18b574420c6f5c1b1bf812d96b68107a3c783f1d
Carlton Joseph <carlton.joseph@gmail.com>
2020-01-05 18:07:17 -0800

    improve dev tool extension handling

---
+++ public/devToolExtDir.js
@@ -0,0 +1,33 @@
+const os = require("os");
+const fs = require("fs");
+const path = require("path");
+
+const devToolMac =
+  "/Library/Application Support/Google/Chrome/Default/Extensions/fmkadmapgofadopljbjfkapdkoienihi/";
+const devToolWin =
+  "/AppData/Local/Google/Chrome/User Data/Default/Extensions/fmkadmapgofadopljbjfkapdkoienihi/";
+const devToolLinux = null; // tbd
+
+const getNewExt = devToolExtP => {
+  const exts = fs.readdirSync(path.join(os.homedir(), devToolExtP));
+  if (exts.length === 1) return exts[0];
+  exts.sort((a, b) => (a > b ? -1 : 1));
+  return exts[0];
+};
+
+const devToolExtDir = () => {
+  const devToolExtSubD =
+    process.platform === "win32"
+      ? devToolWin
+      : process.platform === "darwin"
+      ? devToolMac
+      : process.platform === "linux"
+      ? devToolLinux
+      : null;
+
+  if (!devToolExtSubD) return null;
+
+  return path.join(os.homedir(), devToolExtSubD, getNewExt(devToolExtSubD));
+};
+
+module.exports = devToolExtDir;
--- public/electron.js
+++ public/electron.js
@@ -1,11 +1,11 @@
 const { app, BrowserWindow, Menu, screen } = require("electron");
 const path = require("path");
-const os = require("os");
 const process = require("process");
 const isDev = require("electron-is-dev");
 const WebSocket = require("ws");
 
 const Hm = require("./handleMessages");
+const devToolExtDir = require("./devToolExtDir");
 
 let win;
 
@@ -13,26 +13,6 @@ process.env["ELECTRON_DISABLE_SECURITY_WARNINGS"] = "true";
 const url = isDev
   ? "http://localhost:3000"
   : `file://${path.join(__dirname, "../build/index.html")}`;
-
-const devToolExtPathMac = path.join(
-  os.homedir(),
-  "/Library/Application Support/Google/Chrome/Default/Extensions/fmkadmapgofadopljbjfkapdkoienihi/4.2.1_0"
-);
-const devToolExtPathWin = path.join(
-  os.homedir(),
-  "/AppData/Local/Google/Chrome/User Data/Default/Extensions/fmkadmapgofadopljbjfkapdkoienihi/4.2.1_0"
-);
-let devToolExtPath;
-
-switch (process.platform) {
-  case "win32":
-    devToolExtPath = devToolExtPathWin;
-    break;
-  case "darwin":
-  default:
-    devToolExtPath = devToolExtPathMac;
-    break;
-}
 
 const isMac = process.platform === "darwin";
 
@@ -70,7 +50,9 @@ Menu.setApplicationMenu(menu);
 let wss = null;
 
 function createWindow() {
-  if (isDev) BrowserWindow.addDevToolsExtension(devToolExtPath);
+  const devToolExtPath = devToolExtDir();
+  if (isDev && devToolExtPath)
+    BrowserWindow.addDevToolsExtension(devToolExtPath);
 
   let winOpts = {};
   if (isDev) {
